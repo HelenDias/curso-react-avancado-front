5e1462a91860ce6620dcbe42d3b576189cdaee55
Helen Dias <helendias.hd@gmail.com>
2020-12-20 18:31:03 -0300

    test: add 1st snapshot test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,6 +3,12 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
+  it('should match snapshot', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
   it('should render the heading', () => {
     render(<Main />)
 
